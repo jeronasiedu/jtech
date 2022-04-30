434bfa32b977bd5455635374b221d055b148075e
Jeron <norejudeisa@gmail.com>
2022-04-30 21:37:51 +0000

    implement carousel

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -3,12 +3,14 @@ import { SwiperSlide, Swiper } from 'swiper/react'
 const Gallery = () => {
   const images = [
     { url: '/images/asset1.jpeg', alt: 'carousel image' },
-    { url: '/images/asset1.jpeg', alt: 'carousel image' },
-    { url: '/images/asset1.jpeg', alt: 'carousel image' },
-    { url: '/images/asset1.jpeg', alt: 'carousel image' },
+    { url: '/images/asset2.jpeg', alt: 'carousel image' },
+    { url: '/images/asset3.jpeg', alt: 'carousel image' },
+    { url: '/images/asset4.jpeg', alt: 'carousel image' },
+    { url: '/images/asset5.jpeg', alt: 'carousel image' },
   ]
+
   return (
-    <article>
+    <article className="pb-16">
       <div className="flex flex-col items-center justify-center max-w-3xl px-4 py-16 mx-auto space-y-8 ">
         <h1 className="text-4xl font-bold text-center md:text-5xl lg:text-6xl">
           From rough design files, to {''}
@@ -23,7 +25,7 @@ const Gallery = () => {
       <Swiper
         slidesPerView={1}
         grabCursor
-        // loop={true}
+        spaceBetween={20}
         breakpoints={{
           768: {
             slidesPerView: 2,
@@ -33,16 +35,37 @@ const Gallery = () => {
           },
         }}
       >
-        <SwiperSlide>
-          {images.map((item, idx) => (
-            <figure key={idx} className="relative">
-              <Image src={item.url} alt={item.alt} />
+        {images.map((item, idx) => (
+          <SwiperSlide key={idx}>
+            <figure className="relative w-full h-80 md:h-96 lg:h-[26rem]">
+              <Image
+                src={item.url}
+                alt={item.alt}
+                layout="fill"
+                objectFit="cover"
+              />
             </figure>
-          ))}
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </article>
   )
 }
 
 export default Gallery
+
+function Example({}) {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-3xl px-4 py-16 mx-auto space-y-8 ">
+      <h1 className="text-4xl font-bold text-center md:text-5xl lg:text-6xl">
+        From rough design files, to {''}
+        <span className="text-blue-600 ">powerful products</span>
+      </h1>
+      <p className="text-xl text-center text-gray-700 dark:text-slate-300 ">
+        We can give you the perfect identity with our beautifully designed
+        webpages. A large company needs perfectly designed website for
+        expressing themselves
+      </p>
+    </div>
+  )
+}
